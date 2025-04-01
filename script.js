082137fb923048ee5f8a212a75548ae4a160bc3e
pratiwilestari <pratiwilestari95@gmail.com>
2025-04-01 21:07:22 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.getElementById("dataForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const nama = document.getElementById("nama").value;
-    const catatan = document.getElementById("catatan").value;
-    const status = document.getElementById("status");
+    const descKategori = document.getElementById("descKategori").value;
+    // const catatan = document.getElementById("catatan").value;
+    // const status = document.getElementById("status");
 
     fetch("https://script.google.com/macros/s/AKfycbyp1F5tdIWsFm-UvBLvQcR5CgvIbMdAkZIfYl_7mmTy6SHM1ngFhvPQTQNsFUhqCBRZ/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ nama, catatan }),
+        body: JSON.stringify({ descKategori }),
     })
     .then(response => response.json())
     .then(data => {
